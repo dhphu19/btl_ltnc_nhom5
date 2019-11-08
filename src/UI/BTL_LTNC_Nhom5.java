b442caffefda61ff88a05fff6e3a8ee2df28a1fb
Dinh Duc <nguyendinhduc2408@gmail.com>
2019-11-08 14:30:26 +0700

    upload

---
+++ src/UI/BTL_LTNC_Nhom5.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package UI;
+
+/**
+ *
+ * @author Dinh Duc
+ */
+public class BTL_LTNC_Nhom5 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        //  HELLO LO LO L OL O LO L O O L OL O L O LO L O L OL O L O L O
+        
+        
+        
+    }
+    
+}
